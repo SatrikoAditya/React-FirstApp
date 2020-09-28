7a4f2323e30a83e8687d34ed55b1bef937717105
SatrikoAditya <satriko4d@gmail.com>
2020-09-28 17:53:09 +0700

    class components done

---
+++ football-app/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Teams from './components/Teams'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      teams: []
+    }
+  }
+  
+  componentDidMount() {
+    fetch('http://api.football-data.org/v2/competitions/2021/teams', {
+      headers: {
+        'X-Auth-Token': 'fa3884a581df49b18b6c2c4483114f2a'
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        teams: data.teams
+      })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+
+    const {teams} = this.state
+
+    return (
+      <>
+        <h1 className="mb-4">Premier League Teams</h1>
+        <div className="d-flex flex-row justify-content-center flex-wrap">
+                {
+                    teams.map(teams => {
+                        return (
+                            <Teams team={teams} key={teams.id} />
+                        )
+                    })
+                }
+        </div>
+      </>
+    )
+  }
+}
+
+export default App
---
+++ football-app/src/components/Teams.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Teams extends React.Component {
+    render() {
+        const {crestUrl, name } = this.props.team
+        return (
+            <div className="card border-0 shadow mb-3 col-2 ml-2">
+                <div className="card-body">
+                    <div className="row align-items-center">
+                        <div className="col text-center">
+                            <img
+                                height="80px"
+                                src={crestUrl}
+                                alt="logo-team"
+                            />
+                            <h6 className="font-weight-bold my-3">{name}</h6>
+                            <button className="btn btn-primary">Detail</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Teams
